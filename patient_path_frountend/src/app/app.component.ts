0791e2424e0ba096b0dd4fcb368ceaf4bde9d6f9
Trisha Chowdhury <tchowdhury455@gmail.com>
2024-10-08 21:35:49 +0530

    user dashboard updated

--- patient_path_frountend/src/app/app.component.ts
+++ patient_path_frountend/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>Hello world!</h1>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
